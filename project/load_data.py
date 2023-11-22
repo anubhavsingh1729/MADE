f7d6171c84b90702cf7c339063d54eee6af41c17
anubhavsingh <anubhav.singh1729@gmail.com>
2023-11-22 16:08:34 +0100

    updated load_data.py to include functions for preprocessing

--- project/load_data.py
+++ project/load_data.py
@@ -1,5 +1,8 @@
 
 import requests
+import sqlite3
+import pandas as pd
+
 def get_request(url,output):
     response = requests.get(url)
     if response.status_code == 200:
@@ -10,8 +13,28 @@ def get_request(url,output):
 
 url = 'https://api.worldbank.org/v2/en/indicator/GB.XPD.RSDV.GD.ZS?downloadformat=excel'
 url2 = 'https://hdr.undp.org/sites/default/files/2021-22_HDR/HDR21-22_Statistical_Annex_HDI_Trends_Table.xlsx'
+
 output = '../data/world_bank.xlsx'
 output2 = '../data/HDR.xlsx'
 
+def worlbank_Data(file):
+    dt = pd.read_excel(file,header = 3)
+    dt.dropna(axis=1,how='all')
+    dt.fillna(0,inplace=True)
+    conn = sqlite3.connect('../data/DatasetDB.db')
+    dt.to_sql('worldbank', conn, index=False, if_exists='replace')
+    conn.close()
+
+def HDR(file):
+    dt = pd.read_excel(file,header = 4)
+    dt.dropna(axis=1,how='all',inplace=True)
+    dt=dt[~dt['HDI rank'].isna()]
+    dt.drop('Unnamed: 3',axis=1,inplace=True)
+    conn = sqlite3.connect('../data/DatasetDB.db')
+    dt.to_sql('HDR', conn, index=False, if_exists='replace')
+    conn.close()
+
 get_request(url,output)
 get_request(url2,output2)
+worlbank_Data(output)
+HDR(output2)
